48da6b66199ad38d7ce18cb4639520da9a65f65c
kunwalle <khanglqse@gmail.com>
2019-10-25 14:50:33 +0700

    update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,25 @@
 var webpack = require('webpack');
 var path = require('path');
+// var ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const ASSET_PATH = process.env.ASSET_PATH || '/'
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
     template: path.join(__dirname, "public/index.html"),
     filename: "./index.html"
 });
+// const serviceWorkerPlugin = new ServiceWorkerWebpackPlugin({
+//   entry: path.join(__dirname, 'src/serviceWorkder.js'),
+//   excludes: [
+//     '**/.*',
+//     '**/*.map',
+//     '*.html',
+//   ],
+//   filename: 'serviceWorkder.js'
+// })
+const CopyPlugin = require('copy-webpack-plugin');
+const FilePlugin = new CopyPlugin([
+  {from: 'public', to: '', exclude: 'public/index.html'}
+])
 module.exports = {
     module: {
       rules: [
@@ -18,16 +33,22 @@ module.exports = {
         {
             test: /\.scss$/,
             use: [  'style-loader', 'css-loader','sass-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+          use: {loader:'file-loader'}
         }
       ]
      
      
     },
-    plugins: [htmlWebpackPlugin],
+    plugins: [htmlWebpackPlugin, FilePlugin,  new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+  })],
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'build'),
-      publicPath: '/',
+      publicPath: '',
       filename: 'bundle.js'
     },
     devServer:{
